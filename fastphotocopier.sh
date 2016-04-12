4b7244bb4a780568bedb938363a73e1a342ecea5
kyleschnirring <kyleschnirring@icloud.com>
2016-04-12 09:34:44 -0700

    added progress bar

--- fastphotocopier.sh
+++ fastphotocopier.sh
@@ -2,22 +2,28 @@
 echo "Would you like to run Fast Photo Copier? (type 'y' for yes or 'n' for no). Followed by [Enter]."
 read answer
 if [[ $answer = y ]]; then
-  #statements
   echo "Making File for Fast Photo Copier ..."
   sleep 2
     # Check if directory exists
   if [ -d "~/Desktop/fastphotocopies" ]; then
     # Control will enter here if $DIRECTORY exists.
     echo "File fastphotocopies already exists. No need to copy good bye."
+    exit 1
   else
     mkdir ~/Desktop/fastphotocopies
   fi
     #start copying photos
   sleep 1
   echo "Copying Photos ..."
+  echo -ne '#####                     (33%)\r'
+  sleep 2
+  echo -ne '#############             (66%)\r'
   cp -r ~/Pictures/Photos\ Library.photoslibrary/Masters* ~/Desktop/fastphotocopies
   sleep 3
+  echo -ne '#######################   (100%)\r'
+  echo -ne '\n'
   echo "Done"
 else
   echo "Ok goodbye."
+  exit
 fi
