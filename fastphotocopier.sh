1a7a20e98c2cc685e6d95d76c6ca00f35ca4882e
kyleschnirring <kyleschnirring@icloud.com>
2016-04-12 18:56:14 -0700

    made up more time for large libraries

--- fastphotocopier.sh
+++ fastphotocopier.sh
@@ -13,12 +13,40 @@ if [[ $answer = y ]]; then
     mkdir ~/Desktop/fastphotocopies
   fi
     #start copying photos
+  cp -r ~/Pictures/Photos\ Library.photoslibrary/Masters* ~/Desktop/fastphotocopies
   sleep 1
   echo "Copying Photos ..."
-  echo -ne '#####                     (33%)\r'
+  echo -ne '###                       (11%)\r'
   sleep 2
-  echo -ne '#############             (66%)\r'
-  cp -r ~/Pictures/Photos\ Library.photoslibrary/Masters* ~/Desktop/fastphotocopies
+  echo -ne '#####                     (20%)\r'
+  sleep 2
+  echo -ne '#####                     (26%)\r'
+  sleep 3
+  echo -ne '#######                   (33%)\r'
+  sleep 3
+  echo -ne '#######                   (39%)\r'
+  sleep 2
+  echo -ne '#########                 (44%)\r'
+  sleep 3
+  echo -ne '#########                 (48%)\r'
+  sleep 2
+  echo -ne '###########               (56%)\r'
+  sleep 3
+  echo -ne '#############             (60%)\r'
+  sleep 2
+  echo -ne '#############             (67%)\r'
+  sleep 3
+  echo -ne '###############           (74%)\r'
+  sleep 2
+  echo -ne '#################         (81%)\r'
+  sleep 3
+  echo -ne '#################         (88%)\r'
+  sleep 2
+  echo -ne '###################       (93%)\r'
+  sleep 3
+  echo -ne '###################       (97%)\r'
+  sleep 2
+  echo -ne '####################      (99%)\r'
   sleep 3
   echo -ne '#######################   (100%)\r'
   echo -ne '\n'
